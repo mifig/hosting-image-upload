dc5f94342474da3935b7dbd263c6476f03da3b2d
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2022-11-18 10:55:02 +0000

    added seeds with multiple photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,5 @@ Article.destroy_all
 puts "Creating article.."
 file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
 article = Article.new(title: "NES", body: "A great console")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+article.photos.attach(io: file, filename: "nes.png", content_type: "image/png")
 article.save
